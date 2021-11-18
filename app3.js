fe863b6018f7798f61127ba3978998bd0f85a211
Joshua_McCluskey <jpiff57@gmail.com>
2021-11-17 17:38:32 -0800

    Updated JS file to fix bug for another website to autmatically open

--- app3.js
+++ app3.js
@@ -1,7 +1,7 @@
 let defferal = prompt ("Would you like to visit other local Seattle climbing gyms?")
 
-if (waiverSubmission.toLowerCase() == "yes"){
+if (defferal.toLowerCase() == "yes"){
     window.location.href = "https://edgeworksclimbing.com/";
-} else  {
-    document.write("Please support other local gyms" + <a href = "https://seattleboulderingproject.com/">"SBP"</a> + <a href = "https://seattleboulderingproject.com/">"Edgeworks"</a>);
-    } 
+}  else {
+    document.write("Please visit other local gyms!")
+}
