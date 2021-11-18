ba2486617c91384190b71144c73ab7b06e4f1d70
Joshua_McCluskey <jpiff57@gmail.com>
2021-11-17 17:29:52 -0800

    Added another JS file and updated index to read new JS file and updated style for JS Output

---
+++ app3.js
@@ -0,0 +1,7 @@
+let defferal = prompt ("Would you like to visit other local Seattle climbing gyms?")
+
+if (waiverSubmission.toLowerCase() == "yes"){
+    window.location.href = "https://edgeworksclimbing.com/";
+} else  {
+    document.write("Please support other local gyms" + <a href = "https://seattleboulderingproject.com/">"SBP"</a> + <a href = "https://seattleboulderingproject.com/">"Edgeworks"</a>);
+    } 
