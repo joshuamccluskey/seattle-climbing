9d085158a1a3c3d6fbff7f31c97b930f204ce570
Joshua_McCluskey <jpiff57@gmail.com>
2021-11-17 17:17:37 -0800

    Added another JS file and updated index to read new JS file and updated style for JS Output

--- app2.js
+++ app2.js
@@ -2,6 +2,9 @@
 
 let waiverSubmission = prompt ("Waiver Agreement: The proper climbing and safety training has been provided to you by Seattle Climbing Gym, LLC staff. Seattle Rock Climbing, LLC is not liable for any accident or injury or harm to gym member while participating in rock climbing activities. Please say 'Yes' to confirm and submit waiver or 'No' if you don't agree ");
 
-if (waiverSubmission.toLowerCase == "yes"){
+if (waiverSubmission.toLowerCase() == "yes"){
     document.write(`Waiver Submission Completed Climb On!`);
-} 
+} else  {
+    document.write(`Waiver NOT Submitted! Please see staff member for assistance or visit other Seattle Climbing Gyms`);
+    } 
+
