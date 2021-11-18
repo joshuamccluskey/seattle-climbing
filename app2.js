a15f85268d7e59f074de4294a0e81fe7afac5418
Joshua_McCluskey <jpiff57@gmail.com>
2021-11-18 15:15:13 -0800

    deleted js files consolidated to app1

--- app2.js
+++
@@ -1,5 +0,0 @@
-
-
-// let approvedSubmission = formSubmit ();
-
-
--- app3.js
+++
@@ -1,4 +0,0 @@
-//Defferal prompt that will automatically open another website or remain on current website
-
-// let keepPush = leaveStay ();
-
