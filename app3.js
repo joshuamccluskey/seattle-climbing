74a514d7812f0296b8f489c3c3a54394d092609a
Joshua_McCluskey <jpiff57@gmail.com>
2021-11-17 21:19:52 -0800

    Fixed prompt for clairty of instructions

--- app3.js
+++ app3.js
@@ -1,7 +1,7 @@
-let defferal = prompt ("Would you like to visit other local Seattle climbing gyms? 'No' Stays on Website 'Yes' - Leaves website to another website ) 
+let defferal = prompt ("Would you like to visit other local Seattle climbing gyms? 'No' Stays on Website 'Yes' - Leaves website to another website"); 
 
-if (defferal.toLowerCase() == "yes"){
+if (defferal.toLowerCase() == "yes") {
     window.location.href = "https://edgeworksclimbing.com/";
 }  else {
-    document.write("Please visit other local gyms!")
+    document.write("Please visit other local gyms!");
 }
