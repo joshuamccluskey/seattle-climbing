74dfb9e3fe89803d71f40afaddfee1b0819c432a
Joshua_McCluskey <jpiff57@gmail.com>
2021-11-18 15:13:33 -0800

    Consolidated JS script  to one file and adjusted  index

--- app1.js
+++ app1.js
@@ -7,6 +7,37 @@ function register() {
     console.log(membersName);
 }
 
+
+// Seattle Rock Climbing Website Gym Waiver Sumbmission Prompt
+
+function formSubmit() {
+    let waiverSubmission = prompt ("Waiver Agreement: The proper climbing and safety training has been provided to you by Seattle Climbing Gym, LLC staff. Seattle Rock Climbing, LLC is not liable for any accident or injury or harm to gym member while participating in rock climbing activities. Please say 'Yes' to confirm and submit waiver or 'No' if you don't agree ");
+    console.log(waiverSubmission);
+    //Prompts user of waiver agreement decision and submission
+    if (waiverSubmission.toLowerCase() == "yes"){
+        document.write(`Waiver Submission Completed Climb On!`);
+    } else  {
+        document.write(`Waiver NOT Submitted! Please see staff member for assistance or visit other Seattle Climbing Gyms`);
+        } 
+        return waiverSubmission;
+
+        
+}
+
+
+
+function leaveStay(){
+    let defferal = prompt ("Would you like to visit other local Seattle climbing gyms? 'No' Stays on Website 'Yes' - Leaves website to another website"); 
+    console.log(defferal);
+    //Auto jumps to another website or stays on current website
+    if (defferal.toLowerCase() == "yes") {
+        window.location.href = "https://edgeworksclimbing.com/";
+    }  else {
+        document.write("Please visit other local gyms!");
+    }
+     return defferal;
+}
+
 // let memberLog = register ();
 
 
--- app3.js
+++ app3.js
@@ -1,16 +1,4 @@
 //Defferal prompt that will automatically open another website or remain on current website
 
-function leaveStay(){
-    let defferal = prompt ("Would you like to visit other local Seattle climbing gyms? 'No' Stays on Website 'Yes' - Leaves website to another website"); 
-    console.log(defferal);
-    //Auto jumps to another website or stays on current website
-    if (defferal.toLowerCase() == "yes") {
-        window.location.href = "https://edgeworksclimbing.com/";
-    }  else {
-        document.write("Please visit other local gyms!");
-    }
-     return defferal;
-}
-
 // let keepPush = leaveStay ();
 
