93d0d29680137c294e1e140f6603cc5ec125091d
Joshua_McCluskey <jpiff57@gmail.com>
2021-11-19 03:39:46 -0800

    Built loop for picturePicture function  and fixed errors to number game

--- app1.js
+++ app1.js
@@ -1,24 +1,63 @@
-// Seattle Rock Climbing Website Gym Member Registration Prompt
+function guessingGame(){
+    let userAnswer = prompt('Pick a number 1-10 to see who goes first');
+    let correctAnswer = 9; 
+    let attemps = 4;
 
-
-var slideIndex = 0;
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+    for(let i = 1; i < attemps; i++){
+        while(userAnswer < 1 || userAnswer > 10){
+            userAnswer = prompt('Pick a number 1-10');
+        }
+        if(userAnswer == correctAnswer){
+            alert('You go first!');
+            document.write("Climb On!");
+            break;
+        } else if (userAnswer > correctAnswer){
+            userAnswer = prompt('Too high');
+        } else if (userAnswer < correctAnswer){
+            userAnswer = prompt('Too low');
+        }
+        if(i == 3){
+            alert('The correct answer was ' + correctAnswer);
+            document.write("Me first!");
+        }
+    }
 }
+
+
+
+function picturePicture(){
+    let picture = prompt('How many stars do wanna you give us?');
+    let url = "https://thegraphicsfairy.com/wp-content/uploads/2018/07/Double-Star-Clipart-Image-Blue-GraphicsFairy.jpg";
+    let i = 0;
+        do {
+            i += 1;
+            document.write('<img src="' + url + '" width="100" height="100">');
+        } while (i < picture);
+    }
+
+    
+
+
+
+//Testing a slideshow from a javascript W3schools.com tutorial for slideshows
+// var slideIndex = 0;
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
 
 
 
